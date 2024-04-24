a73d148b474f4bf0c81b880702af3a6216a74338
jrvish <91713981+jrvish@users.noreply.github.com>
2024-04-23 21:29:17 -0600

    Step 3: Appending to the end of test_data.py

--- src/data_check/test_data.py
+++ src/data_check/test_data.py
@@ -63,3 +63,11 @@ def test_similar_neigh_distrib(data: pd.DataFrame, ref_data: pd.DataFrame, kl_th
 ########################################################
 # Implement here test_row_count and test_price_range   #
 ########################################################
+
+# Check that the size of the dataset is not too small or too large 
+def test_row_count(data):
+    assert 15000 < data.shape[0] < 1000000
+
+# Check that the price range is between the min and max prices
+def test_price_range(data: pd.DataFrame, min_price: float, max_price: float):
+    assert data['price'].between(min_price, max_price).all()
