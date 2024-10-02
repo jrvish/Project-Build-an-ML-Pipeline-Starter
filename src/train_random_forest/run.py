c9487654b5c076bea869b0ef7d765bd0023efeb9
jrvish <91713981+jrvish@users.noreply.github.com>
2024-10-01 22:28:13 -0600

    Updated the build pipeline with 2 steps to Pipeline() instead of make_pipeline

--- src/train_random_forest/run.py
+++ src/train_random_forest/run.py
@@ -171,7 +171,7 @@ def get_inference_pipeline(rf_config, max_tfidf_features):
     # Build a pipeline with two steps:
     # 1 - A SimpleImputer(strategy="most_frequent") to impute missing values
     # 2 - A OneHotEncoder() step to encode the variable
-    non_ordinal_categorical_preproc = make_pipeline(
+    non_ordinal_categorical_preproc = Pipeline(
         SimpleImputer(strategy="most_frequent"),
         OneHotEncoder()
     )
